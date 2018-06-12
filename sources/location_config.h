dfcf9ab8b11eaf62a24e77c6b97bbd0d0d832b7e
CodeOhms <hendersonzacdev@gmail.com>
2018-06-12 21:51:35 +1000

    mend

--- sources/location_config.h
+++ sources/location_config.h
@@ -2,7 +2,7 @@
 #define  LOCATION_CONFIG_H
 
 //Set by CMake
-#define CONFIG_PATH "/home/pi/.FanController/FanController.conf"
-#define LOG_PATH    "/home/pi/.FanController/FC_Debug.log"
+#define CONFIG_PATH "/usr/local/etc/.FanController/FanController.conf"
+#define LOG_PATH    "/usr/local/etc/.FanController/FC_Debug.log"
 
 #endif //LOCATION_CONFIG_H
